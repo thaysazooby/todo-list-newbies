2419abb2f4dddd6aa88a60831445e166dae5cc9d
Thaysa Zooby <thaysa.zooby@gmail.com>
2023-04-14 09:45:13 -0300

    JS - Criando a lista de dados

---
+++ tasks.js
@@ -0,0 +1,12 @@
+let taskData = [
+    {
+        id: 1,
+        name: 'Ver se eu to na esquina',
+        toDo: true,
+    },
+    {
+        id: 2,
+        name: 'Dar banho nos gatos',
+        toDo: false,
+    }
+]
