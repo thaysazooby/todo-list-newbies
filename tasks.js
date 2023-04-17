71f34d020707953022910f196b2a4aa4220b0a9f
Thaysa Zooby <thaysa.zooby@gmail.com>
2023-04-17 16:24:04 -0300

    JS - Correção do texto da task

--- tasks.js
+++ tasks.js
@@ -93,6 +93,9 @@ function completeTask(event) {
   const todoIcon = event.target
   todoIcon.classList.add("hidden")
 
+  const text = todoIcon.parentNode.childNodes[2]
+  text.classList.add("risked")
+
   const taskToCompleteId = todoIcon.parentNode.parentNode.id
   const taskToComplete = document.getElementById(taskToCompleteId)
 
@@ -116,6 +119,9 @@ function incompleteTask(event) {
 
   const doneIcon = event.target
   doneIcon.classList.add("hidden")
+
+  const text = doneIcon.parentNode.childNodes[2]
+  text.classList.remove("risked")
 
   const taskToIncompleteId = doneIcon.parentNode.parentNode.id
   const taskToIncomplete = document.getElementById(taskToIncompleteId)
